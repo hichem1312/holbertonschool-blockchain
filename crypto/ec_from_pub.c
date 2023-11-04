517c0d33af8c6c6f12009d37b9a8af4f180dc0f0
hichem <hichem@hichem.com>
2023-11-04 02:14:43 +0100

    a function that creates an EC_KEY structure given a public key

---
+++ crypto/ec_from_pub.c
@@ -0,0 +1,26 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_from_pub - a function that creates an EC_KEY structure given a public key
+ * @pub: contains the public key to be converted
+ * Return: pointer or NULL
+ **/
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
+	EC_KEY *p_key;
+    EC_POINT *point;
+
+	if (!pub)
+		return (NULL);
+
+	p_key = EC_KEY_new_by_curve_name(EC_CURVE);
+	if (!p_key)
+		return (NULL);
+    point = EC_POINT_new(EC_KEY_get0_group(p_key))
+
+	if (!EC_POINT_oct2point(EC_KEY_get0_group(p_key), point, pub, EC_PUB_LEN
+                        , NULL))
+		return (NULL);
+
+	return (p_key);
+}
