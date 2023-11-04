13112670917d714c5da498d919d7f7386eb7aa2b
hichem <hichem@hichem.com>
2023-11-04 02:41:21 +0100

    a function that creates an EC_KEY structure given a public key

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -18,11 +18,18 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 		return (NULL);
 	point = EC_POINT_new(EC_KEY_get0_group(p_key));
 	if (!EC_KEY_set_public_key(p_key, point))
+	{
+		EC_KEY_free(key);
+		EC_POINT_free(point);
 		return (NULL);
+	}
 
 	if (!EC_POINT_oct2point(EC_KEY_get0_group(p_key), point, pub, EC_PUB_LEN
 						, NULL))
+	{
+		EC_KEY_free(key);
+		EC_POINT_free(point);
 		return (NULL);
-
+	}
 	return (p_key);
 }
