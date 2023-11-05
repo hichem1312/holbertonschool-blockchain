fe09247d6f8776dc997716cb4d98761c08ac7289
hichem <hichem@hichem.com>
2023-11-05 03:32:52 +0100

    header file

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -45,5 +45,7 @@ uint8_t *sha256(int8_t const *s, size_t len,
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+				size_t msglen, sig_t *sig);
 
 #endif
