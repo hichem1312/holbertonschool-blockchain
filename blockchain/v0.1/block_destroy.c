9e611146c26a3b06a6ff4c4334e575baae4bbef4
hichem <hichem@hichem.com>
2023-11-18 17:00:43 +0100

    a  function that deletes an existing Block

---
+++ blockchain/v0.1/block_destroy.c
@@ -0,0 +1,13 @@
+#include "blockchain.h"
+
+/**
+ * block_destroy - a function that deletes an existing Block
+ * @block: points to the Block to delete
+ * Return: NULL
+ **/
+void block_destroy(block_t *block)
+{
+	if (!block)
+		return(NULL);
+	free(block);
+}
